f3100b7e1e92ca8028208fcc3824996d0b38a1e2
Michael Irvine <michael@Michaels-iMac.local>
2020-10-29 19:53:54 +1300

    Complete functionality to be used by the UFDL web frontend
    This commit adds/fixes missing functionality to be in line with the
    current backend version and the new UFDL web frontend. This is still
    currently a work in progress and still needs signifcant testing. This
    version, however, is at a point where it is ready to be used in a basic
    capacity with the web frontend.

--- src/ufdl/functional/core/mixin_actions.js
+++ src/ufdl/functional/core/mixin_actions.js
@@ -17,7 +17,8 @@ export async function copy(context, url, pk, params) {
  */
 
 export async function download(context, url, pk, filetype, params) {
-    let response = await context.download(detail_url(url, pk) + "download", {filetype: filetype, ...params});
+    let response = await context.download(detail_url(url, pk) + "download",
+                                          {filetype: filetype, ...params});
 
     return response.body;
 }
@@ -27,19 +28,23 @@ export async function download(context, url, pk, filetype, params) {
  */
 
 export async function add_file(context, url, pk, filename, data) {
-    let response = await context.upload(detail_url(url, pk) + `files/${filename}`, filename, data);
+    let response = await context.upload(detail_url(url, pk)
+                                        + `files/${filename}`,
+                                        filename, data);
 
     return response.json();
 }
 
 export async function get_file(context, url, pk, filename) {
-    let response = await context.download(detail_url(url, pk) + `files/${filename}`);
+    let response = await context.download(detail_url(url, pk)
+                                          + `files/${filename}`);
 
-    return response.body;
+    return response.blob();
 }
 
 export async function delete_file(context, url, pk, filename) {
-    let response = await context.delete_(detail_url(url, pk) + `files/${filename}`);
+    let response = await context.delete_(detail_url(url, pk)
+                                         + `files/${filename}`);
 
     return response.json();
 }
@@ -48,8 +53,32 @@ export async function delete_file(context, url, pk, filename) {
  * MembershipViewSet
  */
 
-export async function add_member(context, url, pk, username, permissions) {
-    let response = await context.post(detail_url(url, pk) + "add-member", {
+export async function add_membership(context, url,
+                                     pk, username,
+                                     permissions = "R") {
+    let response = await context.patch(detail_url(url, pk) + "memberships", {
+        method: "add",
+        username: username,
+        permissions: permissions
+    });
+
+    return response.json();
+}
+
+export async function remove_membership(context, url, pk, username) {
+    let response = await context.patch(detail_url(url, pk) + "memberships", {
+        method: "remove",
+        username: username
+    });
+
+    return response.json();
+}
+
+export async function update_membership(context, url,
+                                        pk, username,
+                                        permissions = "R") {
+    let response = await context.patch(detail_url(url, pk) + "memberships", {
+        method: "update",
         username: username,
         permissions: permissions
     });
