3887eb83b299d6daf066c40dd8a681752b68d900
csterling <coreytsterling@gmail.com>
2021-03-24 11:05:14 +1300

    Added the end-points for job outputs.

---
+++ src/functional/core/jobs/job_output.ts
@@ -0,0 +1,27 @@
+import UFDLServerContext from "../../../UFDLServerContext";
+import {RawJSONObject} from "../../../types";
+import * as base_actions from "../../base_actions";
+import {JOB_OUTPUTS_URL} from "../../../constants";
+import * as mixin_actions from "../mixin_actions";
+
+export async function retrieve(
+    context: UFDLServerContext,
+    pk: number
+): Promise<RawJSONObject> {
+    return base_actions.retrieve(
+        context,
+        JOB_OUTPUTS_URL,
+        pk
+    );
+}
+
+export async function download(
+    context: UFDLServerContext,
+    pk: number
+): Promise<ReadableStream<Uint8Array>> {
+    return mixin_actions.download(
+        context,
+        JOB_OUTPUTS_URL,
+        pk
+    );
+}
